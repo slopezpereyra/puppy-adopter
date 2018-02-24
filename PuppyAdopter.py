080391aa100a204fb484230d5289d490a37b9937
lpereyrasantiago <34422960+lpereyrasantiago@users.noreply.github.com>
2018-02-24 14:16:15 -0300

    Update PuppyAdopter.py

--- PuppyAdopter.py
+++ PuppyAdopter.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-import os, requests, bs4, random, shelve, shutil
+import os, requests, bs4, random, shelve, shutil, sys
 
 def get_cwd(appended_value):
     cwd = os.getcwd()
@@ -9,7 +9,7 @@ def get_cwd(appended_value):
     return cwd
 
 def create_folder():
-    print('Comprobando pelusitas y pulguitas...')
+    print('\nComprobando pelusitas y pulguitas...')
     os.makedirs('Puppies', exist_ok=True)
 
 def download_image(url, request_response):
@@ -29,7 +29,38 @@ def save_pages_in_data(saves_list):
         if os.path.isfile(get_cwd('/saves.dat')) == True:
             page_files = shelve.open('saves')
             page_files['saves'] = saves_list
-            page_files.close()
+            page_files.close()#! /usr/bin/python3
+
+def manipulate_pages_list(list):
+    answer = input('Do you wish to add links to scrap? Type "y" for yes, "n" for no.')
+    if answer == "y":
+        while True:
+            appended_link = input('Please, copy the links here. Type "f" to finish.')
+            if appended_link == "f":
+                break
+            list.append(appended_link)
+    elif answer != "n":
+        print('Please type a valid input next time.')
+        menu()
+
+def menu_and_configure_menu(list):
+    answer = input('Welcome to PuppyAdopter! \n\n Enter "s" to start and "c" to configure the pages list. Type "e" to exit.')
+    if answer == "e":
+        sys.exit(0)
+    elif answer == "s":
+        print('Initialazing the program...')
+    elif answer == "c":
+        while True:
+            appended_link = input('\nPlease, copy the links you want to add to the list here. Type "f" to finish. Type "i" for better info.')
+            if appended_link == "f":
+                break
+            elif appended_link== "i":
+                print("\nIn order to add links for the program to download files from, make sure to copy the whole link of the web page you are interested in.")
+                print("\nTo print uncomplete links will may lead to crashes, bugs or unwanted results.")
+                continue
+            list.append(appended_link)
+    elif answer != "n":
+        print('Please type a valid input next time.')
 
 def get_random_url(web_object, web_object_number):
 
@@ -38,9 +69,11 @@ def get_random_url(web_object, web_object_number):
             random_number = random.randint(0, web_object_number)
             url_variable = web_object[random_number].get('src')
             if not '.jpg' in str(url_variable):
+                print("Debug Log: if not .jpg functioning.")
                 continue
             elif os.path.isfile(get_cwd('/saves.dat')) == True:
                 if str(url_variable) in open('saves.dat', encoding='Latin-1').read():
+                    print("Debug Log: saves.dat lecture occur.")
                     continue
             return url_variable
             break
@@ -84,6 +117,7 @@ def get_page():
 
     alr_dow = []
 
+    menu_and_configure_menu(pages)
     create_folder()
     create_data_file()
     look_images(pages, alr_dow)
